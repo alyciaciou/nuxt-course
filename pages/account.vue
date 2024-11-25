30a6c4495e362b204c213e3491335db36d06cb43
“jiafen” <qw159321@wisenvr.com>
2024-11-25 16:16:15 +0800

    Initial commit

---
+++ pages/account.vue
@@ -0,0 +1,12 @@
+<script setup>
+definePageMeta({
+  layout: 'accountlayout',
+})
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ pages/user/[userId].vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+  layout: 'userlayout',
+});
+</script>
+
+<template>
+    <NuxtPage />
+</template>
+
+<style scoped lang="scss"></style>
